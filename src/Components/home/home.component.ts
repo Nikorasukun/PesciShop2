8cc5861884d1a02d7ab9f7fca5ca1ba3a5e19e9a
Nikolas Panterini <NIKOLAS.PANTERINI@local.ittcesena.it>
2025-02-24 10:48:25 +0100

    ritocchi, da migliorare login e listUtenti

--- src/Components/home/home.component.ts
+++ src/Components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
